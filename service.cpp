853c762b47058a8d9780a445298fe57479ca6260
Per Buer <perbu@includeos.org>
2018-12-14 18:36:42 +0100

    readability

--- service.cpp
+++ service.cpp
@@ -5,19 +5,22 @@
 
 #include "Calculator.h"
 
-float handle_line(net::tcp::Connection_ptr conn, net::tcp::buffer_t buffer ) 
+double handle_line(net::tcp::Connection_ptr conn, net::tcp::buffer_t buffer ) 
 {
     double x = 0.0;
     double y = 0.0;
     double result = 0.0;
     char oper = '+';
 
-    Calculator c;
+    Calculator calc;
 
-    std::string s((char*) buffer->data(),buffer->size());
-    std::stringstream ss(s);
-    ss >> x >> oper >> y;
-    result = c.Calculate(x, oper, y);
+    // make a fancy string from the buffer vector
+    std::string input_string((char*) buffer->data(),buffer->size());
+    // and make a stream of it.
+    std::stringstream input_stream(input_string);
+
+    input_stream >> x >> oper >> y;
+    result = calc.Calculate(x, oper, y);
 
     return(result);
 }
@@ -29,14 +32,15 @@ void Service::start()
 
   server.on_connect([] (auto conn) {
     std::cout << "Connection " << conn->to_string() << " established\n";
-    conn->write("-Welcome to the serverless calculator\n");
-    conn->write("-Please enter the operation to perform. Format: a+b | a-b | a*b | a/b\n" );
+    conn->write("Welcome to the serverless calculator\n");
+    conn->write("Please enter the operation to perform. Format: a+b | a-b | a*b | a/b\n>");
 
     conn->on_read(1024, [conn] (auto buf) {
       float f = 0;
       f = handle_line(conn,buf);
       conn->write(std::to_string(f).append("\n"));
       conn->close(); // close the connection. We're so serverless.
+      std::cout << "Connection " << conn->to_string() << " closed\n";
     });
   });
 }
